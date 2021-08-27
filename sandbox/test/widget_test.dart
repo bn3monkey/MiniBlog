b61f396cf64427c63ae955e9767c163445dc72f3
bn3monkey <bn3monkey@naver.com>
2021-08-27 17:42:44 +0900

    -added some sandbox code

--- sandbox/test/widget_test.dart
+++ sandbox/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sandbox/main.dart';
+import 'package:sandbox/main_ori.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
