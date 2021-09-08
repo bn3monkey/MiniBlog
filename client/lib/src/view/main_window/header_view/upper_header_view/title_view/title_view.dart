79064200edc9b1fac79c351141f46ad9cb9f742e
bn3monkey <bn3monkey@naver.com>
2021-09-08 11:57:31 +0900

    -Add new icon

--- client/lib/src/view/main_window/header_view/upper_header_view/title_view/title_view.dart
+++ client/lib/src/view/main_window/header_view/upper_header_view/title_view/title_view.dart
@@ -1,42 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'korea_monkey_icons.dart';
+import 'package:client/src/icon/korea_monkey_icons.dart';
 
 class TitleView extends StatelessWidget {
-    TitleView({Key? key, required this.size}) : super(key : key);
-    final double size;
+  TitleView({Key? key, required this.size}) : super(key: key);
+  final double size;
 
-    @override
-    Widget build(BuildContext context)
-    {
-        return
-            Row(
-                children : <Widget>[
-                    FittedBox(
-                        fit : BoxFit.scaleDown,
-                        alignment : Alignment.centerLeft,
-                        child : Text(
-                            '조선원숭이 ',
-                            style: TextStyle(
-                                fontSize : size,
-                                fontFamily : 'Shinb7',
-                                color: Colors.white
-                            ),
-                        ),
-                    ),
-                    /*
-                    Image(
-                        image : AssetImage('image/title_icon.png'),
-                        width : 30.0,
-                        height : 30.0,
-                    ),
-                    */
-                    Icon(
-                        KoreaMonkey.korea_monkey,
-                        color : Colors.white,
-                        size : size)                   
-                    
-                ]
-            );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: <Widget>[
+      FittedBox(
+        fit: BoxFit.scaleDown,
+        alignment: Alignment.centerLeft,
+        child: Text(
+          '조선원숭이',
+          style: TextStyle(
+              fontSize: size, fontFamily: 'Shinb7', color: Colors.white),
+        ),
+      ),
+      Icon(KoreaMonkey.title_icon, color: Colors.white, size: size * 0.9)
+    ]);
+  }
 }
